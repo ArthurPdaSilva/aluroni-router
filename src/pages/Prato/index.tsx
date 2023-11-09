d3de7233114124e08828b6c9fc2bfbe4e3d8e323
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-11-09 10:05:51 -0300

    feat: mudanças finais

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,6 +2,8 @@ import styles from "./Prato.module.scss";
 import { useNavigate, useParams } from "react-router-dom";
 import cardapio from "data/cardapio.json";
 import TagsPrato from "components/TagsPrato";
+import NotFound from "pages/NotFound";
+import PaginaPadrao from "components/PaginaPadrao";
 
 export default function Prato() {
   const { id } = useParams();
@@ -9,11 +11,11 @@ export default function Prato() {
   const prato = cardapio.find((item) => item.id === Number(id));
 
   if (!prato) {
-    return <div>Prato não encontrado</div>;
+    return <NotFound />;
   }
 
   return (
-    <>
+    <PaginaPadrao>
       <button className={styles.voltar} onClick={() => navigate(-1)}>{"< Voltar"}</button>
       <section className={styles.container}>
         <h1 className={styles.titulo}>{prato.title}</h1>
@@ -25,6 +27,6 @@ export default function Prato() {
           <TagsPrato {...prato} />
         </div>
       </section>
-    </>
+    </PaginaPadrao>
   );
 }
